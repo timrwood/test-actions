41891ef9884992085481bb54b844886d6f3db83a
Tim Wood <washwithcare@gmail.com>
2019-11-20 18:25:33 -0600

    WIP

--- test/models/brain_test.rb
+++ test/models/brain_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class BrainTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    Brain.create!
+    assert true
+  end
 end
